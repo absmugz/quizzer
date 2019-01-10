644f1fd4b04612211698c0b8fb41abe96da85030
Absolom Mugwagwa <absolom@Absolom-Mugwagwa.local>
2019-01-10 15:00:46 +0200

    quiz component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <quiz-list v-bind:quizzes="quizzes"></quiz-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+
+import QuizList from './components/Quizlist';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    QuizList
+  },
+  data() {
+    return {
+      quizzes:[{
+        title:"Chapter 1",
+        description:"This is a sample description.",
+        cards: [
+          {
+            term:"Same Word",
+            definition:"Some definition"
+          },{
+            term:"Same Word",
+            definition:"Some definition"
+          }
+        ]
+        },{
+        title:"Chapter 2",
+        description:"This is a another sample description.",
+        cards: []
+        }]      
+    }
   }
 }
+
+
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
