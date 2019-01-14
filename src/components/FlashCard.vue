81d4044680e586fa337e5f2d560b5c92ddee7e43
Absolom Mugwagwa <absolom@Absolom-Mugwagwa.local>
2019-01-14 11:30:09 +0200

    finishing off the quiz

---
+++ src/components/FlashCard.vue
@@ -0,0 +1,24 @@
+<template>
+ <div class="ui card">
+     <div class="content">
+         <div class="header">
+             {{card.term}}
+         </div>
+         <div class="description">
+             {{card.definition}}
+         </div>
+     </div>
+ </div>
+</template>
+
+<script>
+import Quiz from './Quiz';
+
+export default {
+props:['card'],
+components: {
+    Quiz
+}
+
+}
+</script>
